240bbc6996ef8eb2259c04f75203e6317e2610de
John321Blacksmith <kuzmichevi321@yandex.ru>
2024-06-19 12:12:59 +0300

    fixes

--- trigger.py
+++ trigger.py
@@ -1,9 +1,9 @@
-from manager import Manager
+from engine.manager import Manager
 from memory.datasets import categories
 
 
 
 manager = Manager(categories)
-
-manager.take_input('i am gonna to test it', 'my collegue has helped me solve the difficult problem', 'i went shopping two days ago')
-print(manager.inputs.unrecognized_inputs[2].category)
+inputs = ('my collegue has helped me solve the difficult problem', 'i went shopping two days ago')
+manager.take_input('i am gonna to test it')
+print(manager.inputs.main_context)
