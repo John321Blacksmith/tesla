f3b5c9e000ea74196e8678a7d21e222e87351b6e
John321Blacksmith <kuzmichevi321@yandex.ru>
2024-06-20 11:07:56 +0300

    fixes

--- trigger.py
+++ trigger.py
@@ -4,6 +4,7 @@ from memory.datasets import categories
 
 
 manager = Manager(categories)
-inputs = ('my collegue has helped me solve the difficult problem', 'i went shopping two days ago')
-manager.take_input('i am gonna to test it')
+inputs = ('my collegue has helped me solve the difficult problem', 'i went shopping two days ago', '673462', '.-/.!@*@^!', 'i am gonna to test it')
+manager.take_input(' ', '6434', '734?1', '%^$!@')
+print([inp.data for inp in manager.inputs])
 print(manager.inputs.main_context)
