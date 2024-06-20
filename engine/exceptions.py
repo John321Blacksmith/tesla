5ad52075b13db92ee53e49c868a9bde1e1c2da93
John321Blacksmith <kuzmichevi321@yandex.ru>
2024-06-20 11:05:20 +0300

    added one more exception

--- engine/exceptions.py
+++ engine/exceptions.py
@@ -1,14 +1,14 @@
-class OnlyDigitsDetectedError(Exception):
-    """
-    Error raised when there are
-    only numbers in the input.
-    """
-    ...
-
-
 class IconsistentInputError(Exception):
     """
     Error is raised when the input
     contains types other than string.
+    """
+    ...
+    
+    
+class EmptyInputsListError(Exception):
+    """
+    Raised when there are not any
+    labels based upon inputs.
     """
     ...
