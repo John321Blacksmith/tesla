23d2a00d39c16dfa9435c690bb29a951bf5fac5c
John321Blacksmith <kuzmichevi321@yandex.ru>
2024-08-06 13:41:23 +0300

    iteration over the literals

--- engine/iteration.py
+++ engine/iteration.py
@@ -1,3 +1,18 @@
 """
 This module contains the iterator tools which will ease performing sequence processing.
 """
+from typing import Iterable
+
+
+def dataset_generator(category_items: set[str]) -> Iterable[str]:
+    """
+    Inspect each pattern from the
+    category.
+    """
+
+
+def literal_data_generator(inp: "Input") -> Iterable[str]: # type: ignore
+    """
+    Inspect each literal in the Input
+    object.
+    """
