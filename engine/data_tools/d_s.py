8e77faf9b401b1ab310668cb3c3214eef963cbd0
John321Blacksmith <kuzmichevi321@gmail.com>
2024-09-19 17:48:27 +0300

    Inputs were divided

--- engine/data_tools/d_s.py
+++ engine/data_tools/d_s.py
@@ -1,4 +1,4 @@
-from typing import Union, List, Iterable, Sized
+from typing import Union, Iterable
 from .exceptions import UnknownInputError
 
 
@@ -36,20 +36,6 @@ class Input:
 		"""
 		return {w.lower() for w in self.data.split(' ') if len(w) > 2 and w.isalpha()}
 
-	@staticmethod
-	def categorize_particles(particles: Iterable[str]) -> tuple[str, int]:
-		"""
-		Take a python object
-		and categorize it based
-		on its particles.
-		"""
-		frequency = FrequencyDict()
-		if len(particles) != 0:
-			for p in particles:
-				if p in frequency: frequency[p] += 1
-				else: frequency[p] = 1
-		return frequency.greatest_pair
-
 	def is_valid(self):
 		"""
 		Ensure the input is not
@@ -84,27 +70,37 @@ class FrequencyDict(dict):
 				if tups[i][1] > possible_tuple[1]:
 					possible_tuple = tups[i]
 
-			return possible_tuple
-		
+			return possible_tuple[0]
 		
 
-class InputsList(list):
+class KnownInputs(list):
 	"""
-	A List of all coming inputs.
-	The contexts and a main context
-	are returned here.
+	A all recognized inputs.
 	"""
-	
+	def categorize_particles(self, particles: Iterable[str]) -> tuple[str, int]:
+		"""
+		Take a python object
+		and categorize it based
+		on its particles.
+		"""
+		frequency = FrequencyDict()
+		if len(particles) != 0:
+			for p in particles:
+				if p in frequency: frequency[p] += 1
+				else: frequency[p] = 1
+		return frequency.greatest_pair
+
 	@property
 	def main_context(self) -> Union[tuple[str, int] | None]:
 		"""
 		Collect context frequencies
 		in a dict and find the main
 		context.
+		
 		:returns: str | list[str] | None
 		"""
 		if len(self.contexts) > 0:
-			result: tuple[str, int] = Input.categorize_particles(self.contexts)
+			result: tuple[str, int] = self.categorize_particles(self.contexts)
 			try:
 				if result[0] == 'unknown':
 					raise UnknownInputError('The main context of the input was not understood')
@@ -120,6 +116,14 @@ class InputsList(list):
 		Collect the categories of
 		all the sentences being
 		taken from input.
+
 		:returns: list[str]
 		"""
 		return [s.category for s in self]
+	
+
+class UnknownInputs(list):
+	"""
+	A list of all unrecognized inputs.
+	"""
+	...
