3293c0d9837d1462846d67c1ee16aaacdcb50788
John321Blacksmith <kuzmichevi321@gmail.com>
2024-09-19 17:46:32 +0300

    refacto

--- engine/central_manager.py
+++ engine/central_manager.py
@@ -3,7 +3,10 @@
 from typing import List
 from .input_management.input_processing import InputManager
 from .storage_management.data_processing import DataManager
-from .data_tools.d_s import InputsList, FrequencyDict, Input
+from .data_tools.d_s import (
+    KnownInputs, UnknownInputs, 
+    FrequencyDict, Input
+)
 
 
 class Manager:
@@ -18,19 +21,19 @@ class Manager:
         self.data_manager = DataManager()
 
     @property
-    def known_inputs(self)-> InputsList:
+    def known_inputs(self)-> KnownInputs:
         return self._known_inputs
     
     @known_inputs.setter
-    def known_inputs(self, inputs: InputsList) -> None:
+    def known_inputs(self, inputs: KnownInputs) -> None:
         self._known_inputs = inputs
     
     @property
-    def unknown_inputs(self) -> InputsList:
+    def unknown_inputs(self) -> UnknownInputs:
         return self._unknown_inputs
     
     @unknown_inputs.setter
-    def unknown_inputs(self, inputs: InputsList) -> None:
+    def unknown_inputs(self, inputs: UnknownInputs) -> None:
         self._unknown_inputs = inputs
     
     def process_input(self, inp: str):
