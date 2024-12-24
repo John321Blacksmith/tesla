8c018d7d44fe16d2039e7c731f7b69a6cb44e0cc
ivan.kuzmichev <ivan.kuzmichev@red-soft.ru>
2024-12-24 19:33:20 +0300

    added methods for data processing

--- engine/storage_management/data_processing.py
+++ engine/storage_management/data_processing.py
@@ -1,4 +1,6 @@
 """API for classified data management"""
+from typing import List
+
 
 
 class DataManager:
@@ -7,7 +9,35 @@ class DataManager:
     oriented work over the processed
     inputs.
     """
+    def __init__(self, storage_path: str) -> None:
+        self.storage_path = storage_path
+
+    def take_data(self, data) -> None:
+        """
+        Take processed data.
+        """
+        ...
     
-    def serialize_data(self): ...
+    def serialize_data(self) -> None:
+        """
+        Serialize data.
+        """
+        ...
     
-    def deserialize_data(self): ...
+    def deserialize_data(self)-> None:
+        """
+        Deserialize data.
+        """
+        ...
+    
+    def save_data(self)-> None:
+        """
+        Save serialized data.
+        """
+        ...
+
+    def retrive_data(self) -> None:
+        """
+        Query data from storage.
+        """
+        ...
